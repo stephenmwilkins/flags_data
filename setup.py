a6292719241fa8aa868fca2a91135d1783281380
Stephen Wilkins <sw376@mps026505.mps.schools.susx.ac.uk>
2023-07-01 16:48:47 +0100

    added setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='FLAGS_data',
+      version='0.1',
+      description='First Light and Assembly of Galaxies data collection',
+      author='Stephen Wilkins',
+      author_email='s.wilkins@sussex.ac.uk',
+     )
